7d32b1c36134faa3581dbc5f3dd0efba273e5cbf
Ramazan Kubalov <k700pp@ya.ru>
2023-11-13 18:19:03 +0300

    Выполнил задание №2

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive
+const title = 'Карлькулятор верстки'
+const screens = 'Простые, Сложные, Интерактивные'
+const screenPrice = 100
+const rollback = 10
+const fullPrice = 600000
+const adaptive = true
 
-alert('Hello')
 
+console.log(typeof(title))
+console.log(typeof(fullPrice))
+console.log(typeof(adaptive))
+console.log(screens.length)
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
 
+const screensArray = screens.toLowerCase().split(', ')
+console.log(screensArray)
+
+console.log(fullPrice * (rollback/100))
